48bc6c49f343b4dd7bfadb3f27f68ae7f806fd89
AkiiraBino <IvanShevchenko082002@gmail.com>
2023-08-26 16:50:45 +0500

    delete bootstrap styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,10 @@ import "./App.css"
 import Header from "./components/Header/Header.jsx"
 import Main from "./components/Main/Main.jsx"
 import Footer from "./components/Footer/Footer.jsx"
-import { Col, Container, Row } from "react-bootstrap";
 
 function App() {
   return (
     <div>
-      <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
-          integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
-          crossorigin="anonymous"
-      />
       <Header></Header>
       <Footer></Footer>
 
